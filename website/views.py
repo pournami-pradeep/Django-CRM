b679ed2ec94a03d2cad85f48acc1180698c4e8ef
pournami <pournami@hifx.co.in>
2025-03-31 16:04:02 +0530

    login API

--- website/views.py
+++ website/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from django.contrib import messages
+from django.contrib.auth import authenticate, login, logout
+from django.shortcuts import redirect, render
 
 # Create your views here.
+
+def home(request):
+    if request.method == "POST":
+        username = request.POST["username"]
+        password = request.POST["password"]
+        user = authenticate(username=username,password=password)
+        if user is not None:
+            login(request,user)
+            messages.success(request,"Logged in successfully.")
+            return redirect('home')
+        messages.success(request,"Error logging in.")
+        return redirect('home')
+    return render(request, 'home.html',{})
