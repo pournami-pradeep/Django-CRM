04be47339bc9090ecd9c65eb224b22c676516edf
Pournami Pradeep <pournamipradeep13@gmail.com>
2025-04-05 08:40:04 +0530

    records listing page implemented

--- website/views.py
+++ website/views.py
@@ -2,11 +2,14 @@ from django.contrib import messages
 from django.contrib.auth import authenticate, login, logout
 from django.shortcuts import redirect, render
 
+from website.models import Record
+
 from .forms import UserSignUpForm
 
 # Create your views here.
 
 def home(request):
+    records = Record.objects.all()
     if request.method == "POST":
         username = request.POST["username"]
         password = request.POST["password"]
@@ -14,7 +17,7 @@ def home(request):
         if user is not None:
             login(request,user)
             messages.success(request,"Logged in successfully.")
-            return redirect('home')
+            return render(request, 'home.html',{"records":records})
         messages.success(request,"Error logging in.")
         return redirect('home')
     return render(request, 'home.html',{})
